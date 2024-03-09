8b5fb1be3de72cb811e013f2b05248390c7ad1f8
Karina Tiurina <rktiurina@gmail.com>
2024-03-09 14:50:52 +0100

    Datasets reading

---
+++ Project1/datasets/read.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from scipy.io import arff
+
+def read_Rice_Cammeo_Osmancik(filepath):
+  """
+  Rice (Cammeo and Osmancik) - https://archive.ics.uci.edu/dataset/545/rice+cammeo+and+osmancik
+  """
+  # 1 - Cammeo
+  # 0 - Osmancik
+  data, meta = arff.loadarff(filepath)
+  df = pd.DataFrame(data)
+  df['Class'] = df['Class'].astype(str)
+  df['Class'] = df['Class'].replace('Cammeo', 1)
+  df['Class'] = df['Class'].replace('Osmancik', 0)
+  y = df['Class'].values
+  X = df.drop(columns=['Class']).values
+  return X, y
+  
+def read_Online_Shoppers_intention(filepath):
+  """
+  Online Shoppers intention Dataset - https://archive.ics.uci.edu/dataset/468/online+shoppers+purchasing+intention+dataset
+  """
+  df = pd.read_csv(filepath, sep=',', encoding='utf-8')
+  df['Month'] = df['Month'].replace('Feb', 2)
+  df['Month'] = df['Month'].replace('Mar', 3)
+  df['Month'] = df['Month'].replace('May', 5)
+  df['Month'] = df['Month'].replace('June', 6)
+  df['Month'] = df['Month'].replace('Jul', 7)
+  df['Month'] = df['Month'].replace('Aug', 8)
+  df['Month'] = df['Month'].replace('Sep', 9)
+  df['Month'] = df['Month'].replace('Oct', 10)
+  df['Month'] = df['Month'].replace('Nov', 11)
+  df['Month'] = df['Month'].replace('Dec', 12)
+  df['VisitorType'] = df['VisitorType'].replace('Returning_Visitor', 1)
+  df['VisitorType'] = df['VisitorType'].replace('New_Visitor', 2)
+  df['VisitorType'] = df['VisitorType'].replace('Other', 0)
+  df['Weekend'] = df['Weekend'].replace(False, 0)
+  df['Weekend'] = df['Weekend'].replace(True, 1)
+  df['Revenue'] = df['Revenue'].replace(False, 0)
+  df['Revenue'] = df['Revenue'].replace(True, 1)
+
+  y = df['Revenue'].values
+  X = df.drop(columns=['Revenue']).values
+  return X, y
